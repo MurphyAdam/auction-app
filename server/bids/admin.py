b452f73c99f6dee659d08cc73ba74966f697c724
Adam Clark <elm.majidi@gmail.com>
2021-09-30 23:07:30 +0100

    changed model name (Bids -> Bid)

--- server/bids/admin.py
+++ server/bids/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Bids
+from .models import Bid
 
-admin.site.register(Bids)
+admin.site.register(Bid)
--- server/bids/serializers.py
+++ server/bids/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Bids
+from .models import Bid
 from users.serializers import UserSerializer
 
 
@@ -7,5 +7,5 @@ class BidsSerializer(serializers.ModelSerializer):
     user = UserSerializer(many=False, read_only=True)
 
     class Meta:
-        model = Bids
+        model = Bid
         fields = ('id', 'amount', 'version', 'user')
