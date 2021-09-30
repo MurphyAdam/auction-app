859c4fe13afe0a60c331da1cd3072f9ddddea005
Adam Clark <elm.majidi@gmail.com>
2021-09-30 21:19:48 +0100

    created UI loaders

---
+++ client/src/components/Common/Loaders.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+    paddingTop: theme.spacing(1),
+  },
+}));
+
+export function CircularLoader(props) {
+  const classes = useStyles();
+
+  return (
+    <Typography 
+      align={props.align || "center"}
+      className={classes.root} component="div">
+      <CircularProgress color="secondary" size={30} thickness={5} disableShrink/>
+    </Typography>
+  );
+}
+
+
+export const BorderLinearProgress = withStyles({
+  root: {
+    height: 10,
+  },
+  bar: {
+    borderRadius: 20,
+  },
+})(LinearProgress);
