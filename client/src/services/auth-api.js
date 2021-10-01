1b60dca77b4afd48226bb894a13ae846f111d15c
Adam Clark <elm.majidi@gmail.com>
2021-10-01 23:20:40 +0100

    created loginService

--- client/src/services/auth-api.js
+++ client/src/services/auth-api.js
@@ -1,3 +1,4 @@
 import api from './api';
 
 export const fetchCurrentUserService = () => api(`/security/current_user/`);
+export const loginService = formData => api.post(`/auth/login/`, {...formData});
