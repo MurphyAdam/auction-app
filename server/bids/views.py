495018e1ef224775fe9a71da057de84a65bb9148
Adam Clark <elm.majidi@gmail.com>
2021-10-01 23:13:12 +0100

    created the BidViewSet

--- server/bids/views.py
+++ server/bids/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import BidsSerializer
+from .models import Bid
+from auction_server.permissions import IsReadOnly
+from products.models import Product
 
-# Create your views here.
+
+class BidViewSet(viewsets.ModelViewSet):
+    serializer_class = BidsSerializer
+    permission_classes = [IsAuthenticated | IsReadOnly]
+
+    def get_queryset(self):
+        return Bid.objects.all()
+
+    def perform_create(self, serializer):
+        data = self.request.data
+        product_id = data.get('product_id', None)
+        product = Product.objects.get(id=product_id)
+        bid = Bid.objects.filter(
+            user=self.request.user, product=product).first()
+        serializer.save(user=self.request.user, product=product, bid=bid)
