ba98e14679250e919adb0fb29b9c75cb46220997
Adam Clark <elm.majidi@gmail.com>
2021-10-02 21:21:39 +0100

    removed heroku settings

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python server/manage.py migrate
-web: gunicorn --chdir server auction_server.wsgi --log-file -
--- server/auction_server/settings.py
+++ server/auction_server/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -99,10 +97,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-DATABASES['default'].update(dj_database_url.config(
-    conn_max_age=500, ssl_require=True))
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -194,6 +188,3 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
