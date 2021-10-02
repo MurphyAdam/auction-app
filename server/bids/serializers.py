623d87cac602639c48e21884c96c11bc9d215780
Adam Clark <elm.majidi@gmail.com>
2021-10-02 11:58:52 +0100

    updated BidsSerializer

--- server/bids/serializers.py
+++ server/bids/serializers.py
@@ -9,21 +9,22 @@ class BidsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Bid
-        fields = ('id', 'amount', 'version', 'auto_bid', 'user')
+        fields = ('id', 'amount', 'version', 'auto_bid', 'updated', 'user')
 
     def create(self, validated_data):
         amount = validated_data.get('amount', Decimal('10.00'))
         product = validated_data.get('product')
         auto_bid = validated_data.get('auto_bid', None)
-        last_bid_version = validated_data.get('version', None)
+        #last_bid_version = validated_data.get('version', None)
         bid = validated_data.pop('bid')
-        print(validated_data)
-        print(bid.version, ' - ',  last_bid_version)
+        last_bid_origin = "native"
+        """
         if bid.version != last_bid_version:
             raise serializers.ValidationError(
-                {'message': f'Could not place your bid of {bid}. \
+                {'message': f'Could not place your bid of {amount}. \
                     Other bids may have taken place. \
                         Please refresh page.'})
+        """
         if product.expired:
             raise serializers.ValidationError(
                 {'message': 'You cannot bid on this product. Bids are finished.'})
@@ -41,13 +42,14 @@ class BidsSerializer(serializers.ModelSerializer):
                         'message':
                             f'Bid amount should be larger than last bid of {product.last_bid_value}'
                     })
-        if product.min_bid >= amount:
+        if product.min_bid > amount:
             raise serializers.ValidationError(
                 {'message': 'Bid amount should be larger than minimum bid'})
         if bid:
             bid.amount = amount
+            bid.last_bid_origin = last_bid_origin
             if auto_bid is not None:
                 bid.auto_bid = auto_bid
             bid.save()
             return bid
-        return Bid.objects.create(**validated_data)
+        return Bid.objects.create(**validated_data, last_bid_origin=last_bid_origin)
