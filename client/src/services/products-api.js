1e97f0570aecb42fa1f71b416cad803c19bb47e6
Adam Clark <elm.majidi@gmail.com>
2021-10-03 11:21:44 +0100

    removed saveSettingsService

--- client/src/services/products-api.js
+++ client/src/services/products-api.js
@@ -4,5 +4,4 @@ export const fetchProductsService = q => api(`/products`, { params: q });
 export const fetchCategoriesService = q => api(`/categories`);
 export const fetchProductByIdService = id => api(`/products/${id}`);
 export const fetchProductBySlugService = slug => api(`/products/${slug}`);
-export const saveSettingsService = settings => api.post(`/settings`, settings);
 export const makeBidService = params => api.post(`/bids`, params);
